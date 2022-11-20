f5271e775bac9f0c41b3a0f5c825a3bf3875639f
mariambhatti <mariambhattiskipq@gmail.com>
2022-11-20 16:51:54 +0500

    Wb health monitoring project

---
+++ mariambhatti/Sprint1/sprint1/sprint1_stack.py
@@ -0,0 +1,27 @@
+from aws_cdk import Stack
+from aws_cdk import aws_lambda as lambda_  # Duration,; aws_sqs as sqs,
+from constructs import Construct
+
+
+class Sprint1Stack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        fn = self.create_lambda("HelloLambda", './resources', 'HelloWorldLambda.lambda_handler')
+
+    def create_lambda(self, id, asset, handler):
+        return lambda_.Function(self,
+        id=id,
+        handler=handler,
+        code=lambda_.Code.from_asset(asset),
+        runtime=lambda_.Runtime.PYTHON_3_9
+
+        )
+
+        # The code that defines your stack goes here
+
+        # example resource
+        # queue = sqs.Queue(
+        #     self, "Sprint1Queue",
+        #     visibility_timeout=Duration.seconds(300),
+        # )
---
+++ mariambhatti/Sprint2/resources/constants.py
@@ -0,0 +1,6 @@
+URL_TO_BE_MONITORED =["skipq.org","youtube.com","google.com","amazon.com"]
+namespace="MariamBhattisprint2Namespace"
+AvailabilityMetric="URL_AVAILABILITY"
+LatencyMetric="URL_LATENCY"
+
+#these are the constants that are to be provided cloudwatch object in Webhealth.py file
