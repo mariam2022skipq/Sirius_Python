f1ec1ca602340e035c7d0eddf461801beb040559
mariambhatti <mariambhattiskipq@gmail.com>
2022-11-28 18:23:54 +0500

    Everything Done

--- mariambhatti/Sprint3/resources/DbApp.py
+++ mariambhatti/Sprint3/resources/DbApp.py
@@ -1,17 +1,19 @@
 import boto3
 import os
+from Db_putdata import Dynamo1
+
 
 def lambda_handler(event, context):
-    client =boto3.resource('dynamodb', region_name='us-east-2')
-    #get the key of the table
-    dbTable = os.environ['AlarmTable']
-    table=client.Table(dbTable)
-    #extracting message ID and timestamp from the JSON file ; parsing the event data
+    #creating a dynamo object of db_putdata
+    dynamo_obj = Dynamo1()
+    #extracting message ID and timestamp from JSOn file
     message_id = event["Records"][0]["Sns"]["MessageId"]
     timestamp = event["Records"][0]["Sns"]["Timestamp"]
-    #passing the event data after parsing to the DB_putdata table
-    #in the put_data_in_dynamoDB function inside dynamo_data class
-    response=table.put_item(Item={"id": message_id, "Timestamp": timestamp})
+
+    #passing the data to be put into message id and time stamp in dynamoDB
+    dynamo_obj.db_put_data(message_id, timestamp)
+    
+
 
 
 
