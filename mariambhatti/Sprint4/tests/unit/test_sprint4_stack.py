ba5e133ecb6f159c0d43cc42bd917c3657693f1c
mariambhatti <mariambhattiskipq@gmail.com>
2022-12-02 23:37:03 +0500

    new

--- mariambhatti/Sprint4/tests/unit/test_sprint4_stack.py
+++ mariambhatti/Sprint4/tests/unit/test_sprint4_stack.py
@@ -9,96 +9,33 @@ from aws_cdk import(aws_dynamodb as db_,)
 from sprint4.sprint4_stack import Sprint4Stack
 
 # code ref: https://docs.aws.amazon.com/cdk/api/v1/python/aws_cdk.assertions/Template.html
-#test 1) Checking the creation of lambda
-#def test_lambda():
-    #instantiating my application
-   # app = core.App()
-    #stack = Sprint4Stack(app, "sprint4")
-    #template = assertions.Template.from_stack(stack)
-    #template.resource_count_is("AWS::Lambda::Function", 4)
-#test2) Checking the creation of SNS topic
-def test_SNS():
-    app = core.App()
-    stack = Sprint4Stack(app, "sprint4")
-    template = assertions.Template.from_stack(stack)
-    template.resource_count_is("AWS::SNS::Topic", 1)
+import requests
+import json
+import uuid
 
-#test3)checking the IAM policy for lambda 
-def test_IAMPolicy():
-    app = core.App()
-    stack = Sprint4Stack(app, "sprint4")
-    template = assertions.Template.from_stack(stack)
-    template.resource_count_is("AWS::IAM::Policy", 1)
+url = "https://2ukag51771.execute-api.us-east-2.amazonaws.com/prod/URLS"
+url_id = str(uuid.uuid4)
 
-#test4) checking the cloudwatch alarm metrics
-def test_CloudWatchMetrics():
-    app = core.App()
-    stack = Sprint4Stack(app, "sprint4")
-    template = assertions.Template.from_stack(stack)
-    template.has_resource_properties("AWS::CloudWatch::Alarm", {
-        "MetricName": "URL_AVAILABILITY",
-        "MetricName": "URL_LATENCY"
-    })
-#test5) checking the SNS subscription
-def test_SNS_Subscription():
-    app = core.App()
-    stack = Sprint4Stack(app, "sprint4")
-    template = assertions.Template.from_stack(stack)
-    template.has_resource_properties("AWS::SNS::Subscription", {
-        "Endpoint": "mariambhattiskipq@gmail.com"
-    })
 
-#test6) Check if dynamo DB table is created or not 
-#def test_Dynamo_Db_existence():
-    #app = core.App()
-    #stack = Sprint4Stack(app, "sprint4")
-    #template = assertions.Template.from_stack(stack)
-    #template.resource_count_is("AWS::DynamoDB::Table", 2)
+def test_http_post_method():
 
-#FUNCTIONAL TESTS
+    website = {"url_id": url_id, "url_name": "www.facebook.com"}
+    r = requests.post(url, data=json.dumps(website))
+    assert r.status_code == 200
 
-#1) Test if the availability function returns only 1 or zero
-def getAvailability():
-    http=urllib3.PoolManager()
-    response=http.request("GET","skipq.org")
-    if response.status==200:
-        return 1.0
-    else:
-        return 0.0
 
-def test_availability():
-    assert getAvailability()==1 or getAvailability()==0
+def test_http_get_method():
+    r = requests.get(url)
+    assert r.status_code == 200
 
-#2) check if latency is less than 1
-def getLatency():
-    http=urllib3.PoolManager()
-    start=datetime.datetime.now()
-    response=http.request("GET","skipq.org")
-    end=datetime.datetime.now()
-    delta=end-start
-    latencySec=round(delta.microseconds* .000001,6)
-    return latencySec
 
-def test_latency():
-    assert getLatency() <1
+def test_http_patch_method():
+    website = {"url_id": url_id, "url_name": "www.twitter.com"}
+    r = requests.patch(url, data=json.dumps(website))
+    assert r.status_code == 200
 
-#3) Check if dynamo Db is creating table correctly
-def create_dynamoDB_table():
-        table = db_.Table("AlarmTable",
-        partition_key=db_.Attribute(name="id", type=db_.AttributeType.STRING),
-        sort_key=db_.Attribute(name="Timestamp",type=db_.AttributeType.STRING))
-        return table
 
-#3) Check if dynamo Db is creating table correctly
-def test_dynamo_table():
-    app=core.App()
-    stack=Sprint4Stack(app, "MariamBhattiStack")
-    table=stack.create_dynamoDB_table
-    assert table is not None
-
-#4) Check if lambda function is created successfully 
-def test_if_lambda_is_created():
-    app=core.App()
-    stack=Sprint4Stack(app, "MariamBhattiStack")
-    lambda_exists=stack.create_lambda
-    assert lambda_exists is not None
+def test_http_delete_method():
+    website = {"url_id": url_id}
+    r = requests.delete(url, data=json.dumps(website))
+    assert r.status_code == 200
