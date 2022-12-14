b5e09afbc74458e5dcced68df5ca9bd8e32e0378
mariambhatti <mariambhattiskipq@gmail.com>
2022-12-14 23:49:06 +0500

    hey

--- mariambhatti/Sprint5/Design5/resources/bucket_handler.py
+++ mariambhatti/Sprint5/Design5/resources/bucket_handler.py
@@ -6,6 +6,9 @@ import re
 from collections import Counter
 #from aws_cdk import aws_s3 as s3_
 
+dynamodb=boto3.resource('dynamodb',region_name='us-east-2')
+table_name = os.environ['bucket_table']
+table = dynamodb.Table(table_name)
 
 def lambda_handler(event, context):
     s3Client = boto3.client('s3')
@@ -17,8 +20,16 @@ def lambda_handler(event, context):
     data=response['Body'].read().decode('utf-8')
     #process the data 
     parsed_data= re.sub(r'\W+', ' ', data)
-    processed_data = parsed_data.split()
-    dic_count = Counter(processed_data)
+    temp = parsed_data.split()
+    word_count={}
+    for i in range(len(temp)):
+        if str(temp[i]) in word_count:
+            word_count[str(temp[i])] +=1
+        else:
+            word_count[str(temp[i])]=1
+    
+    table.put_item(Item={"Filename": str(key), "Word_count": str(word_count)})
+
 
     #https://www.learnaws.org/2020/12/18/aws-ses-boto3-guide/
     #Before sending email using SES, we need to verigy it
@@ -26,46 +37,36 @@ def lambda_handler(event, context):
     response = ses_client.verify_email_identity(
     EmailAddress="mariambhattiskiq@gmail.com" )
 
-    ses_client = boto3.client("ses", region_name="us-west-2")
+
+    ses_client = boto3.client("ses", region_name="us-east-2")
     CHARSET = "UTF-8"
 
     response = ses_client.send_email(
         Destination={
             "ToAddresses": [
-                "mariambhattiskiq@gmail.com",
+                "mariambhattiskipq@gmail.com",
             ],
         },
         Message={
             "Body": {
                 "Text": {
                     "Charset": CHARSET,
-                     "Data": json.dumps(dic_count),
+                    "Data": f"FileName: {key}\nFreq : {word_count} ",
                 }
             },
             "Subject": {
                 "Charset": CHARSET,
-                "Data": json.dumps("Word Count of "+ key),
+                "Data": f"Word Count  in {key}",
             },
         },
-        Source="abhishek@learnaws.org",
+        Source="mariambhattiskipq@gmail.com",
     )
 
+    
 
-    #writing batch data to the table
-    dynamodb=boto3.resource('dynamodb',region_name='us-east-2')
-    table_name = os.environ['bucket_table']
-    table = dynamodb.Table(table_name)
-    for i,j in dic_count.items():
-            response = table.put_item(Item={
-                "Word": i,
-                "Count": str(j),
-                "Filename": key
-            })
-   
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Success Data Sent!')
-    }
+
+    
+    
 
     
        
